885316fc0032600ef9c8c95617ef85a2b3ef7dc0
hakunamoni <hakunamoni@gmail.com>
2021-12-24 15:31:57 +0800

    test approver

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -13,6 +13,7 @@ let campaign;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+    // "accounts" include 10 different account provided by ganache
 
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
@@ -35,5 +36,19 @@ describe('Campaigns', () => {
     it('deploys a factory and a campaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
     });
 });
